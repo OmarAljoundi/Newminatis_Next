2930178fee416e4826c284a2485de1af5f834164
Omar Aljoundi <omaraljoundi@gmail.com>
2023-06-24 16:33:52 +0300

    s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  experimental: {
+    appDir: true,
+  },
   images: {
     minimumCacheTTL: 86400,
     remotePatterns: [
