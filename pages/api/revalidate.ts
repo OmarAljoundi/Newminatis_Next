51b6f16a6f921241524f0a7ab91117bda3d5f49e
Omar Aljoundi <omaraljoundi@gmail.com>
2023-07-27 00:21:50 +0300

    revalidate api accepts multiple paths

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,17 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { revalidatePath } from "next/cache";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
-  const path = req.query.path as string;
+  const body = req.body;
+  body.paths.map(async (path: string) => {
+    await res.revalidate(path);
+  });
 
-  await res.revalidate(path);
-
-  return res.json({ revalidated: true });
+  return res.json({
+    revalidated: true,
+    paths: (body.paths as string[]).join(","),
+  });
 }
+
+export { handler as POST };
