c76c0587b9b6aae29678414175cde455f90e6061
Omar Aljoundi <omaraljoundi@gmail.com>
2023-07-27 00:28:47 +0300

    .

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,19 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    const body = req.body;
+    body.paths.map(async (path: string) => {
+      await res.revalidate(path);
+    });
+
+    return res.json({
+      revalidated: true,
+      paths: (body.paths as string[]).join(","),
+    });
+  } else {
+    return res.json({
+      revalidated: false,
+      paths: "No method was found",
+    });
   }
-
-  const body = req.body;
-  body.paths.map(async (path: string) => {
-    await res.revalidate(path);
-  });
-
-  return res.json({
-    revalidated: true,
-    paths: (body.paths as string[]).join(","),
-  });
 }
-
-export { handler as POST };
