e08361c1fed02b0e3200a84687bb1e1434a67632
Omar Aljoundi <omaraljoundi@gmail.com>
2023-07-31 23:26:33 +0300

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.8,
   exclude: ["/server-sitemap.xml"], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      process.env.SITE_URL + "/server-sitemap.xml", // <==== Add here
+      process.env.NEXT_PUBLIC_SITE_URL + "/server-sitemap.xml", // <==== Add here
     ],
   },
 };
